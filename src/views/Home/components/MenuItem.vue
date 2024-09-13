098ac31c7119805e08712e29c833f80ff6f9e823
lijiangtao <3530812934@qq.com>
2024-09-13 11:02:59 +0800

    每日推荐图标日期处理

--- src/views/Home/components/MenuItem.vue
+++ src/views/Home/components/MenuItem.vue
@@ -1,12 +1,15 @@
 <script setup>
+import { ref } from 'vue'
 defineProps({
   menuList: Array
 })
+// 每日推荐的 日期
+const curDate = ref(new Date().getDate())
 </script>
 <template>
   <div class="menu flex w-[100%] overflow-auto">
     <div
-      class="menu-item w-[12vw] flex-shrink mr-[5vw] flex justify-center flex-col items-center"
+      class="menu-item relative w-[12vw] flex-shrink mr-[5vw] flex justify-center flex-col items-center"
       v-for="item in menuList"
       :key="item"
     >
@@ -16,6 +19,12 @@ defineProps({
         height="8vw"
         :src="item.image.imageUrl"
       />
+      <span
+        v-show="item.mainTitle.title === '每日推荐'"
+        class="date absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-80%] w-[12vw] text-center block text-[3vw]"
+        >{{ curDate }}</span
+      >
+
       <span class="title w-[12vw] text-center block text-[2.564vw]">{{
         item.mainTitle.title
       }}</span>
