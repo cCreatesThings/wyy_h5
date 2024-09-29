0dbcf438816c3c148eb2d4e051f51fb54fc607c9
lijiangtao <3530812934@qq.com>
2024-09-29 22:44:16 +0800

    更新用户地址-首次没有显示字符串

--- src/utils/throttle.js
+++ src/utils/throttle.js
@@ -1,3 +1,5 @@
+import { ref } from 'vue'
+import { showNotify } from 'vant'
 export const throttle = (fn, delay) => {
   let timerId = null
   return function (...args) {
@@ -8,4 +10,21 @@ export const throttle = (fn, delay) => {
       }, delay)
     }
   }
+}
+
+// 节流函数  防止短时间内多次修改
+const lastRequestTime = ref(0)
+const requestInterval = 3 * 60 * 1000 // 3分钟
+export const checkRequestInterval = () => {
+  const currentTime = Date.now()
+  if (currentTime - lastRequestTime.value < requestInterval) {
+    showNotify({
+      message: '请不要频繁修改',
+      background: '#ff976a',
+      type: 'warning'
+    })
+    return false
+  }
+  lastRequestTime.value = currentTime
+  return true
 }
